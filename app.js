3e37edb2d093555e09750272f9b176fbac13e79a
adamb00 <borsodi.dm@gmail.com>
2022-09-05 07:10:22 +0200

    hopefully

--- app.js
+++ app.js
@@ -13,21 +13,26 @@ const cvcOnCard = document.querySelector('.cvcOnCard');
 const yearOnCard = document.querySelector('.yearOnCard');
 const monthOnCard = document.querySelector('.monthOnCard');
 
+btnConfirm.disabled = true;
+document.addEventListener('input', e => {
+  e.preventDefault();
+  if (
+    !inputCVC.value ||
+    !cardNumber.value ||
+    !inputMonth.value ||
+    !inputYear.value ||
+    !cardHolderName.value
+  ) {
+    console.log('lol');
+    btnConfirm.disabled = true;
+  } else {
+    btnConfirm.disabled = false;
+  }
+});
+
 button.forEach(btn => {
   btn.addEventListener('click', e => {
     e.preventDefault();
-
-    if (
-      !inputCVC.value ||
-      cardNumber.value ||
-      inputMonth.value ||
-      inputYear.value ||
-      cardHolderName.value
-    ) {
-      btnConfirm.disabled = true;
-    } else {
-      btnConfirm.disabled = false;
-    }
 
     if (btn.id === 'btnConfirm') {
       form.style.cssText = 'display:none';
@@ -47,6 +52,7 @@ button.forEach(btn => {
       yearOnCard.innerHTML = '';
       cvcOnCard.innerHTML = '';
       monthOnCard.innerHTML = '';
+      btnConfirm.disabled = true;
     }
   });
 });
@@ -75,12 +81,10 @@ nameOnCardFunc = e => {
     cardHolderName.style.borderColor = 'hsl(0, 100%, 66%)';
     let error = document.querySelector('.errorName');
     error.innerHTML = `Can't be blank`;
-    btnConfirm.disabled = true;
     nameOnCard.innerHTML = '';
   } else {
     document.querySelector('.errorName').innerHTML = ``;
     cardHolderName.style.borderColor = 'hsl(249, 99%, 64%)';
-    btnConfirm.disabled = false;
   }
 };
 numberOnCardFunc = e => {
@@ -91,18 +95,15 @@ numberOnCardFunc = e => {
     cardNumber.style.color = 'hsl(0, 100%, 66%)';
     numberOnCard.textContent = '';
     error.innerHTML = `Only number accepted`;
-    btnConfirm.disabled = true;
   } else if (!cardNumber.value) {
     numberOnCard.innerHTML = '';
     error.innerHTML = `Can't be blank`;
     cardNumber.style.borderColor = 'hsl(0, 100%, 66%)';
     cardNumber.style.color = 'hsl(0, 100%, 66%)';
-    btnConfirm.disabled = true;
   } else {
     error.innerHTML = '';
     cardNumber.style.borderColor = 'hsl(249, 99%, 64%)';
     cardNumber.style.color = 'black';
-    btnConfirm.disabled = false;
     numberOnCard.textContent = num
       .replace(/[^\dA-Z]/g, ' ')
       .replace(/(.{4})/g, '$1 ')
@@ -119,19 +120,16 @@ cvcOnCardFunc = e => {
     inputCVC.style.color = 'hsl(0, 100%, 66%)';
     cvcOnCard.textContent = '';
     error.innerHTML = `Only number accepted`;
-    btnConfirm.disabled = true;
   } else if (!inputCVC.value) {
     error.innerHTML = `Can't be blank`;
     cvcOnCard.innerHTML = '';
     inputCVC.style.borderColor = 'hsl(0, 100%, 66%)';
     inputCVC.style.color = 'hsl(0, 100%, 66%)';
-    btnConfirm.disabled = true;
   } else {
     inputCVC.style.borderColor = 'hsl(249, 99%, 64%)';
     inputCVC.style.color = 'black';
     cvcOnCard.textContent = num;
     error.innerHTML = '';
-    btnConfirm.disabled = false;
   }
 };
 monthOnCardFunc = e => {
@@ -141,23 +139,19 @@ monthOnCardFunc = e => {
     inputMonth.style.borderColor = 'hsl(0, 100%, 66%)';
     inputMonth.style.color = 'hsl(0, 100%, 66%)';
     monthOnCard.textContent = '';
-    btnConfirm.disabled = true;
     error.innerHTML = `Only number accepted`;
   } else if (num > 12) {
-    btnConfirm.disabled = true;
     error.innerHTML = `Only 12 month allowed`;
   } else if (!inputMonth.value) {
     monthOnCard.innerHTML = '';
     error.innerHTML = `Can't be blank`;
     inputMonth.style.borderColor = 'hsl(0, 100%, 66%)';
-    btnConfirm.disabled = true;
     inputMonth.style.color = 'hsl(0, 100%, 66%)';
   } else {
     inputMonth.style.borderColor = 'hsl(249, 99%, 64%)';
     inputMonth.style.color = 'black';
     monthOnCard.textContent = num;
     error.innerHTML = '';
-    btnConfirm.disabled = false;
   }
 };
 yearOnCardFunc = e => {
@@ -168,19 +162,16 @@ yearOnCardFunc = e => {
     inputYear.style.borderColor = 'hsl(0, 100%, 66%)';
     inputYear.style.color = 'hsl(0, 100%, 66%)';
     yearOnCard.textContent = '';
-    btnConfirm.disabled = true;
     error.innerHTML = `Only number accepted`;
   } else if (!inputYear.value) {
     yearOnCard.innerHTML = '';
     error.innerHTML = `Can't be blank`;
     inputYear.style.borderColor = 'hsl(0, 100%, 66%)';
-    btnConfirm.disabled = true;
     inputYear.style.color = 'hsl(0, 100%, 66%)';
   } else {
     inputYear.style.borderColor = 'hsl(249, 99%, 64%)';
     inputYear.style.color = 'black';
     yearOnCard.textContent = '/' + num;
     error.innerHTML = '';
-    btnConfirm.disabled = false;
   }
 };
